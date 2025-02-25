51fb4461550f67e880e264b2c34a39d44948767c
Serana <serana.storey@gmail.com>
2025-02-25 12:22:06 +0000

    Fixed running of expressions/questions for the code to process, begun adding boolean questions to debug other files.

--- code/main.py
+++ code/main.py
@@ -2,16 +2,13 @@ from valhalla import Lexer
 from parsed import parse
 from Interp import evaluate
 
-expression = "1 - 2"
-expression = "2.5 + 2.5 - 1.25"
-expression = "(10 * 2) / 6"
-expression = "8.5 / (2 * 9) - -3"
+expression = ["1 - 2", "2.5 + 2.5 - 1.25", "(10 * 2) / 6", "8.5 / (2 * 9) - -3", "true == false", "true != flase"]
+for i in expression:
+    lexer = Lexer(i)
+    tokens = lexer.tokenize()
 
-lexer = Lexer(expression)
-tokens = lexer.tokenize()
+    rpn_expr = parse(tokens)
+    print("RPN Output", rpn_expr)
+    result = evaluate(rpn_expr)
 
-rpn_expr = parse(tokens)
-print("RPN Output", rpn_expr)
-result = evaluate(rpn_expr)
-
-print(result)
+    print(result)
