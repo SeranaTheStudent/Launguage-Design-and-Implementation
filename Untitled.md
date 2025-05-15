896e02728ac5f518d36f1f42f1135dacc0319739
Serana <serana.storey@gmail.com>
2025-05-15 18:57:36 +0100

    Begun re-writing parser.

--- Untitled.md
+++
@@ -1,1 +0,0 @@
-lexer -> Parser -> Execution(interpreter)
--- code/parsed.py
+++ code/parsed.py
@@ -1,6 +1,34 @@
 from collections import deque
-from tokens import TOKENTYPES
+from tokens import TOKENTYPES as tt
 from codetypes import StringLiteral
 
-def parse(tokens):
-    tokens = ['1', '-', '2']
+def ParseError(Exception):
+    def __init__(self,):
+
+class Parser:
+    """
+    program     = declaration* eof ; (begining part)
+
+    declararation = varDec1
+                  | statement ;
+    varDec1       = "var" IDENTIFIER ( "=" expression )? ";" ;
+    statement     = express Statment
+                   | printStmt
+                   | block ;
+    block          = "{" ( declaration )* "}" ; 
+
+    exprStmt       = expression ";" ;
+    printStmt      = "print" expression ";" ;
+
+    expression     = assignment ;
+    assignment     = identifier ( "=" assingment )?
+                   | equality ;
+    equality       -> comparison ( ( "!=" | "==" ) comparison )*
+    comparison     -> term ( ( ">" | ">=" | "<" | "<=" ) term )*
+    term           -> factor ( ( "-" | "+" ) factor )*
+    factor         -> unary ( ( "/" | "*" ) unary )*
+    unary          -> ( "!" | "-" ) unary
+                   | primary
+    primary        -> NUMBER |  
+    """
+
